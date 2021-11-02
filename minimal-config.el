7496cf595765f3dfba1cb0d5fc077b6d16df78c1
Spongiforma <ooh.nooess@gmail.com>
2021-11-02 15:08:17 +0800

    stage for hugo

--- minimal-config.el
+++ minimal-config.el
@@ -1,28 +1,6 @@
-
+(with-eval-after-load 'ox
+  (require 'ox-hugo))
 (setq make-backup-files nil)
-
-(setq org-html-mathjax-template
-   "<script type=\"text/x-mathjax-config\">
-    MathJax.Hub.Config({
-        displayAlign: \"%ALIGN\",
-        displayIndent: \"%INDENT\",
-
-        extensions: [\"[Contrib]/physics/physics.js\"],
-        \"HTML-CSS\": { scale: %SCALE,
-                        linebreaks: { automatic: \"%LINEBREAKS\" },
-                        webFont: \"%FONT\"
-                       },
-        SVG: {scale: %SCALE,
-              linebreaks: { automatic: \"%LINEBREAKS\" },
-              font: \"%FONT\"},
-        NativeMML: {scale: %SCALE},
-        TeX: { equationNumbers: {autoNumber: \"%AUTONUMBER\"},
-               MultLineWidth: \"%MULTLINEWIDTH\",
-               TagSide: \"%TAGSIDE\",
-               TagIndent: \"%TAGINDENT\"
-             }
-});
-</script>
-<script type=\"text/javascript\"
-        src=\"%PATH\"></script>")
 (setq org-latex-packages-alist '(("" "siunitx" t) ("" "physics" t) ("" "bm" t)))
+(setq org-hugo-base-dir "~/hugo/")
+(setq org-hugo-default-section-directory "posts")
