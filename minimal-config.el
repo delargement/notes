8b96d4183b284444d226a46652ca203ed136c4c2
Kannan Vishal <56219994+Spongiforma@users.noreply.github.com>
2021-11-02 16:58:11 +0800

    ci

--- minimal-config.el
+++ minimal-config.el
@@ -1,3 +1,4 @@
 (setq make-backup-files nil)
 (setq org-latex-packages-alist '(("" "siunitx" t) ("" "physics" t) ("" "bm" t)))
 (setq org-hugo-base-dir "/__w/notes/notes/hugo/")
+(setq org-hugo-external-file-extensions-allowed-for-copying nil)
