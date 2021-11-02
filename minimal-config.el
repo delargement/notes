19af68d3e46591f8b57a737bdee6f87a4ef3f289
Kannan Vishal <56219994+Spongiforma@users.noreply.github.com>
2021-11-02 16:18:42 +0800

    update ci

--- minimal-config.el
+++ minimal-config.el
@@ -1,4 +1,3 @@
 (setq make-backup-files nil)
 (setq org-latex-packages-alist '(("" "siunitx" t) ("" "physics" t) ("" "bm" t)))
 (setq org-hugo-base-dir ".")
-(setq org-hugo-default-section-directory "posts")
